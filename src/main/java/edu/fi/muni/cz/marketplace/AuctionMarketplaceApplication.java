28462886879f3f2460c7a5c4e054601008dfe4a7
xkollar3 <505734@mail.muni.cz>
2025-12-14 11:08:46 +0100

    feat: initialize project

---
+++ src/main/java/edu/fi/muni/cz/marketplace/AuctionMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package edu.fi.muni.cz.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionMarketplaceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuctionMarketplaceApplication.class, args);
+  }
+
+}
